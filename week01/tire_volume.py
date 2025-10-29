2091801e5cbf2d3aa62715369280d521f6908684
karolym1 <karolina@karolinalymphotography.com>
2025-10-29 14:20:05 -0600

    updated photo

---
+++ week01/tire_volume.py
@@ -0,0 +1,12 @@
+import math
+from datetime import datetime
+
+width= float(input("Enter the tire width in mm (ex 205): "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter= float(input( "Enter the diameter of the wheel in inches (ex 15): " ))
+
+volume = (math.pi * (width ** 2) * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
+print(f"The approximate volume is {volume:.2f} litters")
+#Current date (without time)
+#Width, aspect ratio, diameter
+#Volume (rounded to 2 decimals)
